3740e3a9b792e96dfd7f64e6fe73feb562f7105c
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 21:17:26 -0300

    validation init

---
+++ shopping-server/src/main/java/io/labs/shoppingserver/validator/UniqueEmailValidator.java
@@ -0,0 +1,17 @@
+package io.labs.shoppingserver.validator;
+
+import io.labs.shoppingserver.annotation.UniqueEmail;
+import io.labs.shoppingserver.repositories.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        return userRepository.findUserByEmail(email) == null;
+    }
+}
