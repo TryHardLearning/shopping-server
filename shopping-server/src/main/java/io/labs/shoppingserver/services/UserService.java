4d81d154e32efc28fc4485820cb374197976a390
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 22:37:02 -0300

    hotfix

--- shopping-server/src/main/java/io/labs/shoppingserver/services/UserService.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/services/UserService.java
@@ -1,12 +1,13 @@
 package io.labs.shoppingserver.services;
 
+import io.labs.shoppingserver.models.User;
 import io.labs.shoppingserver.repositories.UserRepository;
 import io.labs.shoppingserver.services.implement.CrudServiceImpl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService extends CrudServiceImpl<UserRepository, Long> {
+public class UserService extends CrudServiceImpl<User, Long> {
 
     private final UserRepository repository;
 
@@ -15,7 +16,7 @@ public class UserService extends CrudServiceImpl<UserRepository, Long> {
     }
 
     @Override
-    protected JpaRepository<UserRepository, Long> getRepository() {
+    protected JpaRepository<User, Long> getRepository() {
         return this.repository;
     }
 }
