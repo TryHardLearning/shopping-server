4e7db374338806649dcda69ac815633a4c7627d7
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 22:35:27 -0300

    shoppingbag controller enable for use

--- shopping-server/src/main/java/io/labs/shoppingserver/controllers/ShoppingBagController.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/controllers/ShoppingBagController.java
@@ -1,4 +1,31 @@
 package io.labs.shoppingserver.controllers;
 
-public class ShoppingBagController {
+import io.labs.shoppingserver.controllers.implement.AllCrudController;
+import io.labs.shoppingserver.models.DTOs.ShoppingBagDTO;
+import io.labs.shoppingserver.models.ShoppingBag;
+import io.labs.shoppingserver.services.ShoppingBagService;
+import io.labs.shoppingserver.services.implement.CrudServiceImpl;
+import org.modelmapper.ModelMapper;
+
+public class ShoppingBagController extends AllCrudController<ShoppingBag, ShoppingBagDTO, Long> {
+    private final ShoppingBagService service;
+
+    private final ModelMapper modelMapper;
+
+    public ShoppingBagController(ShoppingBagService service, ModelMapper modelMapper) {
+        super(ShoppingBag.class, ShoppingBagDTO.class);
+        this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    protected CrudServiceImpl<ShoppingBag, Long> getService() {
+        return service;
+    }
+
+    @Override
+    protected ModelMapper getModelMapper() {
+        return modelMapper;
+    }
+
 }
