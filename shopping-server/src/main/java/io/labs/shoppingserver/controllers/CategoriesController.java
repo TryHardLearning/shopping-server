b6cd71cadbf2c4f54fe5986a6640d3bda022e387
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 22:22:40 -0300

    categories controller create

--- shopping-server/src/main/java/io/labs/shoppingserver/controllers/CategoriesController.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/controllers/CategoriesController.java
@@ -1,4 +1,30 @@
 package io.labs.shoppingserver.controllers;
 
-public class CategoriesController {
+import io.labs.shoppingserver.controllers.implement.AllCrudController;
+import io.labs.shoppingserver.models.Category;
+import io.labs.shoppingserver.models.DTOs.CategoryDTO;
+import io.labs.shoppingserver.services.implement.CrudServiceImpl;
+import io.labs.shoppingserver.services.CategoriesService;
+import org.modelmapper.ModelMapper;
+
+public class CategoriesController  extends AllCrudController<Category, CategoryDTO,Long> {
+    private final CategoriesService service;
+
+    private final ModelMapper modelMapper;
+
+    public CategoriesController(CategoriesService service, ModelMapper modelMapper) {
+        super(Category.class, CategoryDTO.class);
+        this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    protected CrudServiceImpl<Category, Long> getService() {
+        return service;
+    }
+
+    @Override
+    protected ModelMapper getModelMapper() {
+        return modelMapper;
+    }
 }
