1036d2b69d21f088c877ad7527860ae6cc24e50f
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 22:35:34 -0300

    user controller enable for use

--- shopping-server/src/main/java/io/labs/shoppingserver/controllers/UserController.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/controllers/UserController.java
@@ -1,4 +1,33 @@
 package io.labs.shoppingserver.controllers;
 
-public class UserController {
+import io.labs.shoppingserver.controllers.implement.AllCrudController;
+
+import io.labs.shoppingserver.models.DTOs.UserDTO;
+import io.labs.shoppingserver.models.User;
+import io.labs.shoppingserver.services.UserService;
+import io.labs.shoppingserver.services.implement.CrudServiceImpl;
+import org.modelmapper.ModelMapper;
+
+public class UserController extends AllCrudController<User, UserDTO, Long> {
+
+    private final UserService service;
+
+    private final ModelMapper modelMapper;
+
+    public UserController(UserService service, ModelMapper modelMapper) {
+        super(User.class, UserDTO.class);
+        this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    protected CrudServiceImpl<User, Long> getService() {
+        return service;
+    }
+
+    @Override
+    protected ModelMapper getModelMapper() {
+        return modelMapper;
+    }
+
 }
