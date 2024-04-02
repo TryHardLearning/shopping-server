5e627d2e2d074ee70dd89709072a90ffdd7ef72c
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 21:14:52 -0300

    shopping bag repository and service

---
+++ shopping-server/src/main/java/io/labs/shoppingserver/services/ShoppingBagService.java
@@ -0,0 +1,23 @@
+package io.labs.shoppingserver.services;
+
+import io.labs.shoppingserver.models.Category;
+import io.labs.shoppingserver.models.ShoppingBag;
+import io.labs.shoppingserver.repositories.ShoppingBagRepository;
+import io.labs.shoppingserver.services.CRUD.CrudServiceImpl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShoppingBagService extends CrudServiceImpl<ShoppingBag, Long> {
+
+    private ShoppingBagRepository repository;
+
+    public ShoppingBagService(ShoppingBagRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    protected JpaRepository<ShoppingBag, Long> getRepository() {
+        return this.repository;
+    }
+}
