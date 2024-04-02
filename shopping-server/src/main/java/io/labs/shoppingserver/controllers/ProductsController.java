d7a0832c77995db3a828294cf66da032bcadcff6
Lucas Labs <lucascaladob@gmail.com>
2024-04-01 22:35:12 -0300

    products controller enable for use

--- shopping-server/src/main/java/io/labs/shoppingserver/controllers/ProductsController.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/controllers/ProductsController.java
@@ -1,4 +1,32 @@
 package io.labs.shoppingserver.controllers;
 
-public class ProductsController {
+import io.labs.shoppingserver.controllers.implement.AllCrudController;
+
+import io.labs.shoppingserver.models.DTOs.ProductDTO;
+import io.labs.shoppingserver.models.Product;
+import io.labs.shoppingserver.services.ProductsService;
+import io.labs.shoppingserver.services.implement.CrudServiceImpl;
+import org.modelmapper.ModelMapper;
+
+public class ProductsController extends AllCrudController<Product, ProductDTO, Long> {
+    private final ProductsService service;
+
+    private final ModelMapper modelMapper;
+
+    public ProductsController(ProductsService service, ModelMapper modelMapper) {
+        super(Product.class, ProductDTO.class);
+        this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    protected CrudServiceImpl<Product, Long> getService() {
+        return service;
+    }
+
+    @Override
+    protected ModelMapper getModelMapper() {
+        return modelMapper;
+    }
+
 }
