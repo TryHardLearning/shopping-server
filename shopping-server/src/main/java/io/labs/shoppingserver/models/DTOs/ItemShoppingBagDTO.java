d686fbfc2d52f727f6d2d4c83f195f8b7f42b436
Lucas Labs <lucascaladob@gmail.com>
2024-05-06 14:38:50 -0300

    item shopping bag correction

--- shopping-server/src/main/java/io/labs/shoppingserver/models/DTOs/ItemShoppingBagDTO.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/models/DTOs/ItemShoppingBagDTO.java
@@ -1,27 +1,30 @@
 package io.labs.shoppingserver.models.DTOs;
 
+import io.labs.shoppingserver.models.Product;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ItemShoppingBagDTO {
-    @Id
+
     Long id;
 
-    @OneToOne
-    private ProductDTO product;
+    @OneToOne @NotNull
+    private Product product;
 
+    @NotNull
     private Integer quantity;
 
-    private Double Cost;
+    @NotNull
+    private Double cost;
 }
--- shopping-server/src/main/java/io/labs/shoppingserver/models/ItemShoppingBag.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/models/ItemShoppingBag.java
@@ -1,28 +1,25 @@
 package io.labs.shoppingserver.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ItemShoppingBag {
-    @Id
-    Long id;
 
-    @OneToOne
+    @OneToOne @NotNull
     private Product product;
 
+    @NotNull
     private Integer quantity;
 
-    private Double Cost;
+    @NotNull
+    private Double cost;
 }
