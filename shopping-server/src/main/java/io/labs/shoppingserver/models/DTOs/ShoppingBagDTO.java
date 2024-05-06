450f7f9dd9dd98662409bd8598b2d3bb5ee098da
Lucas Labs <lucascaladob@gmail.com>
2024-05-06 14:42:18 -0300

    shopping bag correction

--- shopping-server/src/main/java/io/labs/shoppingserver/models/DTOs/ShoppingBagDTO.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/models/DTOs/ShoppingBagDTO.java
@@ -1,13 +1,27 @@
 package io.labs.shoppingserver.models.DTOs;
 
+import io.labs.shoppingserver.models.ItemShoppingBag;
+import io.labs.shoppingserver.models.User;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShoppingBagDTO {
 
-    @OneToMany
-    List<ItemShoppingBagDTO> allProductItems;
+    @OneToMany @NotNull
+    List<ItemShoppingBag> ProductItems;
 
-    private UserDTO user;
+    @ManyToOne
+    @NotNull
+    private User user;
 }
--- shopping-server/src/main/java/io/labs/shoppingserver/models/ShoppingBag.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/models/ShoppingBag.java
@@ -1,13 +1,27 @@
 package io.labs.shoppingserver.models;
 
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShoppingBag {
 
-    @OneToMany
-    List<ItemShoppingBag> allProductItems;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @OneToMany @NotNull
+    List<ItemShoppingBag> ProductItems;
+
+    @ManyToOne @NotNull
     private User user;
 }
