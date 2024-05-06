4da54f36d720b4a488d24b7d4d8a2f6e8dfdd8a0
Lucas Labs <lucascaladob@gmail.com>
2024-05-06 14:42:37 -0300

    user correction

--- shopping-server/src/main/java/io/labs/shoppingserver/models/DTOs/UserDTO.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/models/DTOs/UserDTO.java
@@ -1,24 +1,32 @@
 package io.labs.shoppingserver.models.DTOs;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import io.labs.shoppingserver.annotation.UniqueEmail;
 
-@Entity(name = "Costumer")
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDTO {
-    @Id
+
     private Long id;
 
-    private String name;
+    @NotNull @Size(min = 6)
+    private String username;
 
-    private String email;
-
+    @NotNull
+    @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
+            message = "{br.edu.pb.utfpr.pw25s.server.user.password.Pattern}")
     private String password;
+
+    @NotNull
+    @Size(min = 4, max = 255)
+    private String displayName;
+
+    // Remove UserDetails methods as they're not relevant for DTOs
 }
--- shopping-server/src/main/java/io/labs/shoppingserver/models/User.java
+++ shopping-server/src/main/java/io/labs/shoppingserver/models/User.java
@@ -1,24 +1,65 @@
 package io.labs.shoppingserver.models;
 
+import io.labs.shoppingserver.annotation.UniqueEmail;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity(name = "Costumer")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
-    @Id
+public class User implements UserDetails{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @UniqueEmail @NotNull @Size(min = 6)
+    private String username;
 
-    private String email;
-
+    @NotNull
+    @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
+            message = "{br.edu.pb.utfpr.pw25s.server.user.password.Pattern}")
     private String password;
+
+    @NotNull
+    @Size(min = 4, max = 255)
+    private String displayName;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("ROLE_USER");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
